6cbed1d3cfa84e669f0854d2d300bff4c0f928f0
ckb <ckingbailey@gmail.com>
2019-10-18 23:33:46 -0400

    build step g2g

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 let replace = require('gulp-replace')
-let { src, dest } = require('gulp')
+let del = require('del')
+let { src, dest, series } = require('gulp')
 
-exports.default = function replaceServerURL() {
-    return src('client/GithubClient.js')
+function clean() {
+    return del('build')
+}
+
+function build() {
+    return src('client/**/*')
+    .pipe(dest('build/'))
+}
+
+function replaceVars() {
+    return src('build/GithubClient.js')
     .pipe(replace(/(?<=const TOKEN_SERVER = ).+/g, "'https://us-central1-github-commit-schema.cloudfunctions.net/token'"))
     .pipe(dest('build/'))
 }
+
+exports.default = series(clean, build, replaceVars),
+exports.clean = clean
