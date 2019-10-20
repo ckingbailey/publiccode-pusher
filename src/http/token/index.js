21406ff9d432473286ff1600f2c035b82e4bf23c
ckb <ckingbailey@gmail.com>
2019-10-19 23:18:33 -0400

    change the way ghClient is imported coz gcloud is dumb

--- src/http/token/index.js
+++ src/http/token/index.js
@@ -1,12 +1,12 @@
-let ghClient = require('github-client')
+let ghClient = require('./github-client')
 
 exports.token = async (req, res) => {
     console.log(req.body)
     if (req.method !== 'POST')
-        return res.status(405).send('nop')
+        return res.status(405).json({ message: 'nop' })
 
     if (!req.body.code)
-        return res.status(400).send({ message: 'pearljam.com/music/album/no-code' })
+        return res.status(400).json({ message: 'pearljam.com/music/album/no-code' })
 
     let gh = new ghClient(process.env.GH_CLIENT_ID, process.env.GH_CLIENT_SECRET)
 
@@ -18,14 +18,24 @@ exports.token = async (req, res) => {
         if (response.error) throw response
     } catch (error) {
         /**
+         * example error:
          * { error: 'bad_verification_code',
          * error_description: 'The code passed is incorrect or expired.' }
          */
         // what kinds of errors might come from GH?
         // what is error code for "token expired"?
-        let status = +error.status || 500
+        let status = +error.status
+        if (!status && error.error === 'bad_verification_code')
+            status = 400
+        
+        if (!error.message)
+            error = {
+                code: error.error,
+                message: error.error_description
+            }
+
         console.error('Error:', error)
-        return res.status(status).send({ error: error.error_description })
+        return res.status(status).json(error)
     }
 
     res.json(response)
