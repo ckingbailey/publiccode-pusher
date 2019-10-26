115f08d9761ee91dd0e32e4d985fdd322fb88cae
ckb <ckingbailey@gmail.com>
2019-10-26 15:25:22 -0700

    ug promise-based chai testing library

--- test/github-client.test.js
+++ test/github-client.test.js
@@ -1,9 +1,14 @@
 let GhClient = require('../src/http/token/github-client')
-let { expect } = require('chai')
+let chai = require('chai')
+var chaiAsPromised = require("chai-as-promised")
 let proxyquire = require('proxyquire')
 let sinon = require('sinon')
 
+chai.use(chaiAsPromised)
+let { expect } = chai
+
 describe('server-side GithubClient', function() {
+
     describe('constuctor', function() {
         it('throws when not passed a client_id', function() {
             expect(() => new GhClient(null, 3)).to.throw()
@@ -20,21 +25,24 @@ describe('server-side GithubClient', function() {
     })
 
     describe('getToken', function() {
-        // TODO: make this a sinon fake instead
-        let rpStub = sinon.stub().callsFake(function({ uri, method, headers, body, json }) {
+        it('throws when not passed a `code`', async function() {
+            let gh = new GhClient(3, 3)
+            return expect(gh.getToken()).to.be.rejectedWith('code is undefined')
+        })
+        
+        it('calls rp once and returns the response', async function() {
+            let rpStub = sinon.stub().callsFake(function({ uri, method, headers, body, json }) {
                 return new Promise((resolve, reject) => {
                     resolve({ access_token: 12345 })
                     reject({ error: 'oops' })
                 })
             })
-        let GhClient = proxyquire('../src/http/token/github-client',
-            {
-                'request-promise': rpStub
-            })
-
-        it('calls rp once and returns the response', async function() {
+            let GhClient = proxyquire('../src/http/token/github-client',
+                { 'request-promise': rpStub })
             let gh = new GhClient(3, 3)
+            
             let token = await gh.getToken(555)
+
             expect(rpStub.callCount).to.equal(1)
             expect(token['access_token']).to.equal(12345)
         })
