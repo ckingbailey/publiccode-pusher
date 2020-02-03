7f04dd2e38eb41659041ede9c9a1463faaf925d3
ckb <ckingbailey@gmail.com>
2020-02-02 21:38:01 -0800

    janky error display

--- client/src/app/components/Login.js
+++ client/src/app/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from "react-redux";
 
 import { setStateToken, setAuthToken, exchangeStateAndCodeForToken } from '../store/auth'
@@ -89,14 +89,30 @@ class Login extends Component {
 
     render() {
         let qs = `client_id=8390933a81635970d3b6&state=${this.state.ghStateToken}&scope=public_repo%20read:user`
+        let errorMessage = this.props.authError && (
+            <div style={{
+                backgroundColor: '#e80801',
+                width: '100%',
+                position: 'absolute',
+                padding: '.25rem',
+                color: 'white',
+                textAlign: 'center'
+            }}>
+                <h1 className="notify-title">There was a problem signing into GitHub</h1>
+                <p>{ this.props.authError }</p>
+            </div>
+        )
 
         return (
-            <div>
-                <a
-                    href={ `https://github.com/login/oauth/authorize?${qs}` }
-                    className="editor_button editor_button--primary"
-                >Login to GitHub</a>
-            </div>
+            <Fragment>
+                { errorMessage }
+                <div>
+                    <a
+                        href={ `https://github.com/login/oauth/authorize?${qs}` }
+                        className="editor_button editor_button--primary"
+                    >Login to GitHub</a>
+                </div>
+            </Fragment>
         )
     }
 }
